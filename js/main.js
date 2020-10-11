e2b16c6d417d504ec82a0d2aeadd44bb897fe24e
DfoxEr <dfox.cup@yandex.ru>
2020-10-11 18:37:07 +0600

    Rere

--- js/main.js
+++ js/main.js
@@ -5,10 +5,13 @@ const cartClose = document.querySelector('#cart-close');
 
 cartButton.addEventListener('click', function(event){
     cartModal.classList.toggle("is-open");
+    document.body.classList.toggle("locked");
 });
 
 cartClose.addEventListener('click', function(event){
     cartModal.classList.toggle("is-open");
+    document.body.classList.toggle("locked");
+
 });
 
 new WOW().init();
