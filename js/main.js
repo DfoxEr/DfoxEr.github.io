d1dcde4f45a485a7adaf56ab3233cc979dea41c0
DfoxEr <dfox.cup@yandex.ru>
2020-10-21 20:00:37 +0600

    Rere

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ $(document).ready(function(){
         dots: true,
         speed: 300,
         arrows: false,
+        adaptiveHeight: true,
+        asNavFor: '.slider-sync',
+        autoplay: true,
+        autoplaySpeed: 7000
+    });
+    $('.slider-sync').slick({
+        arrows: false,
+        speed: 300,
+        fade: true,
+        swipe: false,
         adaptiveHeight: true
     });
 });
